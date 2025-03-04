6cfb88b2aed34adb043e8ed4487492f8cd3b9649
humphreyokoth <humpasp@gmail.com>
2025-03-04 16:38:40 +0300

    feat:basics

---
+++ csharp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+           Console.WriteLine("How many times do i want to say hello? ");
+           int times = Convert.ToInt32(Console.ReadLine());
+
+           if (times <= 0)
+           {
+            Console.WriteLine("You must enter a number greater than 0");
+           }
+           else
+           {
+            for (int i = 0; i < times; i++)
+            {
+                Console.WriteLine("Hello");
+            }
+           
+           }
+        }
+    }
+}
---
+++ whileLoop/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace whileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many times do i want to say hello? ");
+            int times = Convert.ToInt32(Console.ReadLine());
+
+            if (times <= 0)
+            {
+                Console.WriteLine("You must enter a number greater than 0");
+            }
+            else
+            {
+                int i = 0;
+                while (i < times)
+                {
+                    Console.WriteLine("Hello");
+                    i++;
+                }
+            }
+        }
+    }
+}
