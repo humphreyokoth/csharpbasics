8364bb75e338278c1c4d8f783bb5dd65b0df5a4d
humphreyokoth <humpasp@gmail.com>
2025-03-05 17:19:58 +0300

    refactor: improve FizzBuzz logic for clarity and readability

--- fizzbuzz/Program.cs
+++ fizzbuzz/Program.cs
@@ -1,24 +1,47 @@
 ﻿using System;
 
-namespace  fizzbuz
+namespace fizzbuz
 {
 
     class Program
     {
-        static void Main(string[]args)
+        static void Main(string[] args)
         {
             Console.WriteLine("Enter a number");
-            int number = Convert.ToInt32(Console.ReadLine());
+            // int number = Convert.ToInt32(Console.ReadLine());
 
-            for (int i = 0; i < number; i++)
+            // for (int i = 0; i < 15; i++)
+            // {
+            //     if(i % 3 == 0 && i % 5 ==0){
+            //         Console.WriteLine("FizzBuzz");
+            //     }else if( i % 3 == 0){
+            //         Console.WriteLine("Fizz");
+            //     }else if(i % 5 == 0){
+            //         Console.WriteLine("Buzz");
+            //     }else{
+            //         Console.WriteLine(i);
+            //     }
+            // }
+            bool divThree = false;
+            bool divFive = false;
+            for (int i = 0; i < 15; i++)
             {
-                if(i % 3 == 0 && i % 5 ==0){
+                divThree = i % 3 == 0;
+                divFive = i % 5 == 0;
+                if (divThree && divFive)
+                {
                     Console.WriteLine("FizzBuzz");
-                }else if( i % 3 == 0){
+                }
+                else if (divThree)
+                {
                     Console.WriteLine("Fizz");
-                }else if(i % 5 == 0){
+                }
+                else if (divFive)
+                {
                     Console.WriteLine("Buzz");
-                }else{
+                }
+                else
+                {
                     Console.WriteLine(i);
                 }
             }
--- fizzbuzz/obj/Debug/net8.0/fizzbuzz.AssemblyInfo.cs
+++ fizzbuzz/obj/Debug/net8.0/fizzbuzz.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("fizzbuzz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d9eaddd6880bc82badbc0b0415d00ac9b910fc2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f353883a0de875a3a5e34aedd3df47f220b1694")]
 [assembly: System.Reflection.AssemblyProductAttribute("fizzbuzz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("fizzbuzz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
