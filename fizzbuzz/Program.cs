0d9eaddd6880bc82badbc0b0415d00ac9b910fc2
humphreyokoth <humpasp@gmail.com>
2025-03-05 15:35:24 +0300

    feat: add fizzbuzz project with basic functionality

---
+++ fizzbuzz/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace  fizzbuz
+{
+
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Enter a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < number; i++)
+            {
+                if(i > 0 && i % 3 == 0 && i % 5 ==0){
+                    Console.WriteLine("FizzBuzz");
+                }else if(i > 0 && i % 3 == 0){
+                    Console.WriteLine("Fizz");
+                }else if(i > 0 && i % 5 == 0){
+                    Console.WriteLine("Buzz");
+                }else{
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
