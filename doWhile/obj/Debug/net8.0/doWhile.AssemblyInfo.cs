4181f64b570fd10dc1747380c03f98b467457a12
humphreyokoth <humpasp@gmail.com>
2025-03-11 10:44:57 +0300

    feat: initialize stack project with basic structure and configuration

--- doWhile/obj/Debug/net8.0/doWhile.AssemblyInfo.cs
+++ doWhile/obj/Debug/net8.0/doWhile.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("doWhile")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b63a875f7cf5e5c556e3082c7be8aa0700e27adc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59f12f98f6ba32c70330562b16b055e710385781")]
 [assembly: System.Reflection.AssemblyProductAttribute("doWhile")]
 [assembly: System.Reflection.AssemblyTitleAttribute("doWhile")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
